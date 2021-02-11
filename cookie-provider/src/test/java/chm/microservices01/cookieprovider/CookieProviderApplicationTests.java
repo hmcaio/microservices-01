7d9b2fe85a5324bc2393c1bbab558ae95c70c413
caioh <Caio.HidekiMatsumoto@amdocs.com>
2021-02-11 00:23:35 -0300

    Initial setup of Cookie Provider microservice.

---
+++ cookie-provider/src/test/java/chm/microservices01/cookieprovider/CookieProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package chm.microservices01.cookieprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookieProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
